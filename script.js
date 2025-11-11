792d446cf65e73ea158e2a75abd144617bdc1792
Mahyar Mousavinia <mahyar.mousavi.1408@gmail.com>
2025-11-11 19:11:55 +0400

    Completed The Bankist application (Meesed up code version)

--- script.js
+++ script.js
@@ -7,18 +7,20 @@ const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   dates: [
-    '2023/07/15 - 14:23',
-    '2024/02/28 - 09:45',
-    '2023/11/03 - 18:12',
-    '2024/05/19 - 22:30',
-    '2023/09/08 - 07:55',
-    '2024/01/25 - 16:40',
-    '2023/12/12 - 11:15',
-    '2024/03/07 - 20:05',
+    '2023-07-15T14:23:00',
+    '2024-02-28T09:45:00',
+    '2025-11-11T20:05:00',
+    '2023-11-03T18:12:00',
+    '2025-11-01T22:30:00',
+    '2025-11-06T07:55:00',
+    '2025-11-09T16:40:00',
+    '2025-11-10T11:15:00',
   ],
   interestRate: 1.2, // %
   pin: 1111,
   type: 'premium',
+  locale: 'en-US',
+  currency: 'USD',
 };
 
 const account2 = {
@@ -26,50 +28,59 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   dates: [
-    '2023/06/21 - 13:20',
-    '2024/04/14 - 08:30',
-    '2023/10/29 - 19:45',
-    '2024/02/03 - 23:10',
-    '2023/08/17 - 06:25',
-    '2024/01/08 - 15:50',
-    '2023/12/05 - 10:35',
-    '2024/03/22 - 21:00',
+    '2023-06-21T13:20:00',
+    '2024-04-14T08:30:00',
+    '2023-10-29T19:45:00',
+    '2024-02-03T23:10:00',
+    '2023-08-17T06:25:00',
+    '2024-01-08T15:50:00',
+    '2023-12-05T10:35:00',
+    '2024-03-22T21:00:00',
   ],
   pin: 2222,
   type: 'standard',
+  locale: 'el-GR',
+  currency: 'EUR',
 };
 
 const account3 = {
   owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   dates: [
-    '2023/05/30 - 12:18',
-    '2024/02/14 - 07:42',
-    '2023/11/19 - 17:28',
-    '2024/04/08 - 22:53',
-    '2023/09/26 - 05:37',
-    '2024/01/30 - 14:22',
-    '2023/12/28 - 09:47',
-    '2024/03/15 - 19:12',
+    '2023-05-30T12:18:00',
+    '2024-02-14T07:42:00',
+    '2023-11-19T17:28:00',
+    '2024-04-08T22:53:00',
+    '2023-09-26T05:37:00',
+    '2024-01-30T14:22:00',
+    '2023-12-28T09:47:00',
+    '2024-03-15T19:12:00',
   ],
   interestRate: 0.7,
   pin: 3333,
   type: 'premium',
+  locale: 'fa-IR',
+  currency: 'IRR',
 };
 
 const account4 = {
   owner: 'Sarah Smith',
   movements: [430, 1000, 700, 50, 90],
   dates: [
-    '2023/07/10 - 11:25',
-    '2024/02/22 - 16:40',
-    '2023/10/15 - 08:55',
-    '2024/05/05 - 21:30',
-    '2023/08/29 - 13:45',
+    '2023-05-30T12:18:00',
+    '2024-02-14T07:42:00',
+    '2023-11-19T17:28:00',
+    '2024-04-08T22:53:00',
+    '2023-09-26T05:37:00',
+    '2024-01-30T14:22:00',
+    '2023-12-28T09:47:00',
+    '2024-03-15T19:12:00',
   ],
   interestRate: 1,
   pin: 4444,
   type: 'basic',
+  locale: 'zh-HK',
+  currency: 'HKD',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -143,6 +154,36 @@ const createUsername = function (accounts) {
 };
 createUsername(accounts);
 
+// FIXME Need a function to display timer
+const startLogOutTimer = function () {
+  // DESC set time to 5 minutes
+  let time = 300;
+
+  // NOTE the reason of tick() function is to start the countdown immediately after logging in
+  const tick = function () {
+    const min = Math.floor(time / 60);
+    const sec = time % 60;
+
+    // DESC in each call, print the remaining time to UI
+    labelTimer.textContent =
+      `${min}`.padStart(2, 0) + ':' + `${sec}`.padStart(2, 0);
+
+    // DESC when 0 seconds, stop timer and logout user
+    if (time === 0) {
+      clearInterval(timer);
+      // DESC Display UI ane Message
+      labelWelcome.textContent = `Log in to get started`;
+      containerApp.style.opacity = 0;
+    }
+    time--;
+  };
+  // DESC call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+
+  return timer;
+};
+
 // FIXME Need a function to calculate the current time when user logs in
 const updateTime = function () {
   const now = new Date();
@@ -156,28 +197,90 @@ const updateTime = function () {
 
   return `${year}/${month}/${day} - ${hour}:${minute}`;
 };
-labelDate.textContent = updateTime();
+
+// FIXME Need a function to turn dates into local date based on region
+const formatMovementDate = function (date, locale) {
+  const calcDaysPassed = date1 =>
+    Math.abs((date1 - Date.now()) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(date).getTime());
+
+  if (daysPassed < 1)
+    return (
+      'Today' +
+      ' - ' +
+      `${new Date(date).getHours()}`.padStart(2, 0) +
+      ':' +
+      `${new Date(date).getMinutes()}`.padStart(2, 0)
+    );
+  if (1 <= daysPassed && daysPassed < 2)
+    return (
+      'Yesterday' +
+      ' - ' +
+      `${new Date(date).getHours()}`.padStart(2, 0) +
+      ':' +
+      `${new Date(date).getMinutes()}`.padStart(2, 0)
+    );
+  if (daysPassed <= 7) return `${Math.floor(daysPassed)} days ago.`;
+  else {
+    // const year = new Date(date).getFullYear();
+    // const month = `${new Date(date).getMonth()}`.padStart(2, 0);
+    // const day = `${new Date(date).getDate()}`.padStart(2, 0);
+    // const hour = `${new Date(date).getHours()}`.padStart(2, 0);
+    // const minute = `${new Date(date).getMinutes()}`.padStart(2, 0);
+
+    // return `${year}/${month}/${day} - ${hour}:${minute}`;
+    return new Intl.DateTimeFormat(locale).format(new Date(date));
+  }
+};
+
+// FIXME Need a function to turn currencies into local currency value of the account region
+const formattedMovements = function (account, moves) {
+  return new Intl.NumberFormat(account.locale, {
+    style: 'currency',
+    currency: account.currency,
+  }).format(moves);
+};
 
 // FIXME Need a function which shows the transactions of each user
 const displayMovements = function (account, sort = false) {
-  const movements = account.movements;
-  const dates = account.dates;
-  const moves = sort ? movements.toSorted((a, b) => a - b) : movements;
+  const combinedMovesDates = account.movements.map((mov, i) => {
+    return { movement: mov, movementDate: account.dates.at(i) };
+  });
+
+  // TODO turn the transaction amounts into the 'locale' for each account ??????
+
+  // DESC at first, all the reports are sorted from present to past
+  const sortedByDatesFirst = combinedMovesDates.sort(
+    (a, b) => new Date(a.movementDate) - new Date(b.movementDate)
+  );
+
+  // DESC if sort === true: sort the report based on transaction amount $
+  const moves = sort
+    ? sortedByDatesFirst.toSorted((a, b) => a.movement - b.movement)
+    : sortedByDatesFirst;
 
   // NOTE First, the transactions should be empty:
   // DESC innerHTML targets whole tags inside the elements.
   containerMovements.innerHTML = '';
   // NOTE Then, it should show the new user account transaction details:
-  moves.forEach((move, index) => {
-    const type = move > 0 ? 'deposit' : 'withdrawal';
+  moves.forEach((obj, index) => {
+    const { movement, movementDate } = obj;
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
 
     const rowHTML = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       index + 1
     } ${type}</div>
-        <div class="movements__date">${dates[index]}</div>
-        <div class="movements__value">${move.toFixed(2)}€</div>
+        <div class="movements__date">${formatMovementDate(
+          movementDate,
+          account.locale
+        )}</div>
+        <div class="movements__value">${formattedMovements(
+          account,
+          movement
+        )}</div>
       </div>
         `;
     // DESC the following method insert the html to the tag
@@ -198,7 +301,10 @@ const displayMovements = function (account, sort = false) {
 // FIXME Need a function to calculate and show the balance
 const calcDisplayBalance = function (account) {
   account.balance = account.movements.reduce((acc, cur) => acc + cur, 0);
-  labelBalance.textContent = `${account.balance.toFixed(2)}€`;
+  labelBalance.textContent = `${formattedMovements(
+    account,
+    account.balance.toFixed(2)
+  )}`;
 };
 
 // FIXME Need a function to calculate and display the summary for "IN", "OUT", "INTEREST"
@@ -221,9 +327,18 @@ const calcDisplaySummary = function (account) {
       0
     );
 
-  labelSumIn.textContent = `${IN.toFixed(2)}€`;
-  labelSumOut.textContent = `${Math.abs(OUT).toFixed(2)}€`;
-  labelSumInterest.textContent = `${INTEREST.toFixed(2)}€`;
+  labelSumIn.textContent = `${formattedMovements(
+    account,
+    Math.abs(IN).toFixed(2)
+  )}`;
+  labelSumOut.textContent = `${formattedMovements(
+    account,
+    Math.abs(OUT).toFixed(2)
+  )}`;
+  labelSumInterest.textContent = `${formattedMovements(
+    account,
+    INTEREST.toFixed(2)
+  )}`;
 };
 
 // FIXME Need a function that does all the updates in display the summary, balance, movements
@@ -241,7 +356,7 @@ const updateUI = function (account) {
 // TODO Event handlers
 
 // NOTE we should first set the current account to undefined
-let currentAccount;
+let currentAccount, timer;
 
 // FIXME "Login Button" When user logs in, its account must show up
 btnLogin.addEventListener('click', function (e) {
@@ -252,6 +367,7 @@ btnLogin.addEventListener('click', function (e) {
     acc => acc.username === inputLoginUsername.value.trim()
   );
 
+  // DESC if user logs in:
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
     toast('User Logged in', 'info');
 
@@ -259,11 +375,31 @@ btnLogin.addEventListener('click', function (e) {
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
     }`;
-    containerApp.style.opacity = 10;
+    containerApp.style.opacity = 1;
     // DESC after logged in, the input field must be empty (cancel the display of username and password)
     inputLoginUsername.value = inputLoginPin.value = '';
 
+    // DESC Initiation of Timer:
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
+
+    // DESC Specifying the present time for labelDate:
+    const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    };
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(new Date());
+
+    // DESC Updating the user account
     updateUI(currentAccount);
+
     // DESC the cursor on PIN or USER input, should lose its focus; therefore, the keyboard won't type in there
     inputLoginPin.blur();
     inputLoginUsername.blur();
@@ -291,8 +427,8 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-Number(transferValue));
     receiverAccount.movements.push(Number(transferValue));
 
-    currentAccount.dates.push(updateTime());
-    receiverAccount.dates.push(updateTime());
+    currentAccount.dates.push(new Date().toISOString());
+    receiverAccount.dates.push(new Date().toISOString());
 
     // DESC the cursor on PIN or USER input, should lose its focus; therefore, the keyboard won't type in there
     inputTransferTo.blur();
@@ -352,9 +488,11 @@ btnLoan.addEventListener('click', function (e) {
     loanAmount > 0 &&
     currentAccount.movements.some(move => move >= loanAmount * 0.1)
   ) {
-    currentAccount.movements.push(loanAmount);
-    currentAccount.dates.push(updateTime());
-    updateUI(currentAccount);
+    setTimeout(() => {
+      currentAccount.movements.push(loanAmount);
+      currentAccount.dates.push(new Date().toISOString());
+      updateUI(currentAccount);
+    }, 2500);
 
     toast('Loan is transferred successfully', 'success');
   } else {
